0f00075851984c28f7c18925693da81d2c481110
anna <ank@code.ru>
2023-12-17 13:37:01 +0700

    high-level-step-objects

--- pages/registration_page.py
+++ pages/registration_page.py
@@ -4,61 +4,40 @@ import os.path
 
 class RegistrationPage:
 
-    def open_form(self):
+    def register(self, user):
         browser.open('/automation-practice-form')
         browser.element('.pattern-backgound').should(have.exact_text('Practice Form')).click()
+        browser.element('#firstName').should(be.blank).type(user.first_name)
+        browser.element('#lastName').should(be.blank).type(user.last_name)
+        browser.element('#userEmail').should(be.blank).type(user.email)
 
-    def fill_first_name(self, first_name):
-        browser.element('#firstName').should(be.blank).type(first_name)
-
-    def fill_second_name(self, second_name):
-        browser.element('#lastName').should(be.blank).type(second_name)
-
-    def fill_email(self, email):
-        browser.element('#userEmail').should(be.blank).type(email)
-
-    def select_gender(self, gender="Male"):
-        if gender == "Male":
+        if user.gender == "Male":
             gender_selector = 'label[for="gender-radio-1"]'
         else:
             gender_selector = 'label[for="gender-radio-2"]'
+
         browser.element(gender_selector).click()
-
-    def fill_phone(self, phone):
-        browser.element('#userNumber').should(be.blank).type(phone)
-
-    def select_date_of_birth(self, year, month, day):
+        browser.element('#userNumber').should(be.blank).type(user.phone)
         browser.element('#dateOfBirthInput').click()
         browser.element('.react-datepicker__year-select').click()
-        browser.element(by.text(year)).click()
+        browser.element(by.text(user.year)).click()
         browser.element('.react-datepicker__month-select').click()
-        browser.element(by.text(month)).click()
-        browser.element('.react-datepicker__day--0' + day).click()
+        browser.element(by.text(user.month)).click()
+        browser.element('.react-datepicker__day--0' + user.day).click()
+        browser.element('#subjectsInput').should(be.blank).type(user.subject).press_enter()
 
-    def fill_subject(self, subject):
-        browser.element('#subjectsInput').should(be.blank).type(subject).press_enter()
+        for hobby in user.hobbies:
+            if hobby == "Sports":
+                browser.element("[for='hobbies-checkbox-1']").click()
+            elif hobby == "Reading":
+                browser.element("[for='hobbies-checkbox-2']").click()
+            elif hobby == "Music":
+                browser.element("[for='hobbies-checkbox-3']").click()
 
-    def select_hobbie(self, hobby):
-        if hobby == "Sports":
-            browser.element("[for='hobbies-checkbox-1']").click()
-        elif hobby == "Reading":
-            browser.element("[for='hobbies-checkbox-2']").click()
-        elif hobby == "Music":
-            browser.element("[for='hobbies-checkbox-3']").click()
-
-    def upload_picture(self, picture):
-        browser.element('#uploadPicture').send_keys(os.path.abspath(picture))
-
-    def fill_address(self, address):
-        browser.element('#currentAddress').should(be.blank).type(address)
-
-    def select_state(self, state):
-        browser.element('#react-select-3-input').type(state).press_enter()
-
-    def select_city(self, city):
-        browser.element('#react-select-4-input').type(city).press_enter()
-
-    def submit(self):
+        browser.element('#uploadPicture').send_keys(os.path.abspath(user.picture))
+        browser.element('#currentAddress').should(be.blank).type(user.address)
+        browser.element('#react-select-3-input').type(user.state).press_enter()
+        browser.element('#react-select-4-input').type(user.city).press_enter()
         browser.element('#submit').press_enter()
 
     def close_modal(self):
@@ -70,12 +49,11 @@ class RegistrationPage:
     def read_table_data(self):
         return browser.all('.table td:nth-child(2)')
 
-    def should_have_registered(self, first_name, last_name, email, gender, phone, day, month, year, subject,
-                           hobby_1, hobby_2, picture, address, state, city):
+    def should_have_registered(self, user):
 
         self.read_modal_header().should(have.text("Thanks for submitting the form"))
 
         self.read_table_data().should(
-            have.texts(f'{first_name} {last_name}', email, gender, phone, f'{day} {month},{year}', subject,
-                       f'{hobby_1}, {hobby_2}', picture, address, f'{state} {city}'))
+            have.texts(f'{user.first_name} {user.last_name}', user.email, user.gender, user.phone, f'{user.day} {user.month},{user.year}', user.subject,
+                       ", ".join(user.hobbies), user.picture, user.address, f'{user.state} {user.city}'))
 
--- test_registration_page.py
+++ test_registration_page.py
@@ -1,47 +1,28 @@
 from pages.registration_page import RegistrationPage
-from selene import have
+from user.user import User
+
+my_test_user = User(
+    first_name="Anna",
+    last_name="MyLastName",
+    email="kaktus54au@gmail.com",
+    gender="Female",
+    phone="9138018444",
+    year="1983",
+    month="April",
+    day="09",
+    subject="Biology",
+    hobbies=["Sports", "Music"],
+    picture="kitty.jpeg",
+    address="Tomsk Any Street, 123",
+    state="Rajasthan",
+    city="Jaipur",
+)
+
 
 def test_reg_from():
-    first_name = "Anna"
-    last_name = "MyLastName"
-    email = "kaktus54au@gmail.com"
-    gender = "Female"
-    phone = "9138018444"
-    year = "1983"
-    month = "April"
-    day = "09"
-    subject = "Biology"
-    hobby_1 = "Sports"
-    hobby_2 = "Music"
-    picture = "kitty.jpeg"
-    address = "Tomsk Any Street, 123"
-    state = "Rajasthan"
-    city = "Jaipur"
-
     page = RegistrationPage()
-    page.open_form()
+    page.register(my_test_user)
 
-    page.fill_first_name(first_name)
-    page.fill_second_name(last_name)
-    page.fill_email(email)
-    page.select_gender(gender)
-    page.fill_phone(phone)
-    page.select_date_of_birth(year, month, day)
-
-    page.fill_subject(subject)
-    page.select_hobbie(hobby_1)
-    page.select_hobbie(hobby_2)
-
-    page.upload_picture(picture)
-
-    page.fill_address(address)
-    page.select_state(state)
-    page.select_city(city)
-
-    page.submit()
-
-    page.should_have_registered(first_name, last_name, email, gender, phone, day, month, year, subject,
-                           hobby_1, hobby_2, picture, address, state, city)
+    page.should_have_registered(my_test_user)
 
     page.close_modal()
-
